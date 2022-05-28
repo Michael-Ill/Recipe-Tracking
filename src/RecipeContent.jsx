50b3d93e78d5cd1d344d2e4cacef46d8e38daec9
Mike <scnd30@gmail.com>
2022-05-28 01:44:31 -0400

    fixed key error

--- src/RecipeContent.jsx
+++ src/RecipeContent.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 
-
 function RecipeContent({ recipes, setRecipes }) {
-    const handleDelete = (name) => {
-      const filtered = recipes.filter(recipe => recipe.name !== name) 
-      setRecipes(filtered)
-    }
-  
-    return (
+  const handleDelete = (name) => {
+    const filtered = recipes.filter((recipe) => recipe.name !== name);
+    setRecipes(filtered);
+  };
+
+  return (
     <tbody>
-      {recipes.map(({ name, cuisine, photo, ingredients, preparation }) => (
-        <tr>
-          <td>{name}</td>
-          <td>{cuisine}</td>
-          <td>
-            <img src={photo} />
-          </td>
-          <td className="content_td"><p>{ingredients}</p></td>
-          <td className="content_td"><p>{preparation}</p></td>
-          <td>
-            <button name="delete" onClick={() => handleDelete(name)}>Delete</button>
-          </td>
-        </tr>
-      ))}
+      {recipes.map(
+        ({ name, cuisine, photo, ingredients, preparation }, index) => (
+          <tr key={index}>
+            <td>{name}</td>
+            <td>{cuisine}</td>
+            <td>
+              <img src={photo} alt="" />
+            </td>
+            <td className="content_td">
+              <p>{ingredients}</p>
+            </td>
+            <td className="content_td">
+              <p>{preparation}</p>
+            </td>
+            <td>
+              <button name="delete" onClick={() => handleDelete(name)}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        )
+      )}
     </tbody>
   );
 }
