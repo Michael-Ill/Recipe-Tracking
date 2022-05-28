f74d531c3a147715c6a3385544b6b7547e6d21cf
Mike <scnd30@gmail.com>
2022-05-27 20:44:46 -0400

    Updates

---
+++ precache-manifest.1af81b01a5f640b58e90519d5b647359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a46051ef0fe9a3071b9c5d5d78a426",
+    "url": "/Michael-Ill/Recipe-Tracking/index.html"
+  },
+  {
+    "revision": "b5ab690e3a17d52f0bdd",
+    "url": "/Michael-Ill/Recipe-Tracking/static/css/main.5a2b48c2.chunk.css"
+  },
+  {
+    "revision": "8e5211d92102f09916f8",
+    "url": "/Michael-Ill/Recipe-Tracking/static/js/2.b74aa156.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Michael-Ill/Recipe-Tracking/static/js/2.b74aa156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ab690e3a17d52f0bdd",
+    "url": "/Michael-Ill/Recipe-Tracking/static/js/main.fa0e6390.chunk.js"
+  },
+  {
+    "revision": "dd72509c7074d2509261",
+    "url": "/Michael-Ill/Recipe-Tracking/static/js/runtime-main.83056878.js"
+  }
+]);
